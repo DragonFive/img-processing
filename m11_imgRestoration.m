104708581ffdc3e73333b89d3c5b4bf83fd0c42e
DragonFive <dragonfive2013@163.com>
2015-12-08 23:46:32 +0800

    加入运动噪声

--- m11_imgRestoration.m
+++ m11_imgRestoration.m
@@ -6,7 +6,7 @@
 %  $Date: 2015/12/08 17:06:44$
 % -------------------------------------------------------------------
 clear all;
-f=checkerboard(8);
+f=checkerboard(8);%生成一个8格的板子
 g=pixeldup(f,8);
 % whos f;
 % whos g;
@@ -14,3 +14,15 @@ g=pixeldup(f,8);
 % myImshow(pixeldup(f,8),'扩展后');
 % myImshow(0);
 
+%  H = fspecial('motion',LEN,THETA) returns a filter to approximate, once
+%  convolved with an image, the linear motion of a camera by LEN pixels,
+%  with an angle of THETA degrees in a counter-clockwise direction. The
+%  filter becomes a vector for horizontal and vertical motions.  The
+%  default LEN is 9, the default THETA is 0, which corresponds to a
+%  horizontal motion of 9 pixels.
+PSF = fspecial('motion',7,45);
+gb=imfilter(f,PSF,'circular');
+noise=imnoise(zeros(size(f)),'gaussian',0,0.001);
+gbn=gb+noise;
+myImshow(f,'拓展图');myImshow(gb,'运动后图像');myImshow(noise,'噪声图像');
+myImshow(gbn,'运动后噪声图像');myImshow(0);
